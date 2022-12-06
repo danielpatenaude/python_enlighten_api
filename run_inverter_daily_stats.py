374b2810b3482be1c71cbd83f792204b868f1991
Daniel Patenaude <danielbottomley@gmail.com>
2022-12-06 12:49:43 -0500

    Ports existing functionality to v4 API

--- run_inverter_daily_stats.py
+++ run_inverter_daily_stats.py
@@ -1,81 +1,70 @@
 # Author:   Daniel Patenaude
-# Date:     10/13/2020
-# Desc:     Use the Enphase Enlighten API application to pull inverter/panel daily energy production data
+# Date:     12/02/2022
+# Desc:     Use the Enphase Enlighten v4 API application to pull inverter/panel daily energy production data
 
 import datetime
 import json
-import requests
 import os
-from utils.enlightenAPI import enlightenAPI
+from utils.enlightenAPI_v4 import enlightenAPI_v4
 import populate_google_sheet
 
-#-------------------------------------------
-# User Settings
-#-------------------------------------------
-configs = \
-[{
-    "name": "<some name>",
-    "user_id": "<user_id>",
-    "site_id": "<site_id>",
-    "api_url": "<api_url>",
-    "api_key": "<api_key>",
-    "spreadsheet_id": "<google sheet_id>"
-}]
+# Load the user config
+with open('enlighten_v4_config.json') as config_file:
+    config = json.load(config_file)
 
-for config in configs:
-    print(f'Beginning EnlightenAPI pull for site {config["site_id"]}')
-    api = enlightenAPI(config)
+print(f'Beginning EnlightenAPI pull for System ID: {config["system_id"]}')
+api = enlightenAPI_v4(config)
 
-    # Get the inverter data from the enlighten API
-    inverter_summary = api.inverter_summary()[0]
+# Get the inverter data from the enlighten API
+inverter_summary = api.inverter_summary()[0]
 
-    # Read in the existing data or create the folder/file if needed.
-    # This should be formatted as:
-    '''
-    {
-        "micro_inverters" : {
-            "<ID>": { 
-                "<DATE>": {
-                    "daily_energy" <Wh>, 
-                    "lifetime_energy": <Wh> 
-            }
+# Read in the existing data or create the folder/file if needed.
+# This should be formatted as:
+'''
+{
+    "micro_inverters" : {
+        "<ID>": { 
+            "<DATE>": {
+                "daily_energy" <Wh>, 
+                "lifetime_energy": <Wh> 
         }
     }
-    '''
-    inverter_historical_data = {}
-    datafile = f'data/inverter_daily_data-{config["site_id"]}.json'
-    if os.path.isfile(datafile) and os.access(datafile, os.R_OK):
-        with open(datafile) as json_file:
-            inverter_historical_data = json.load(json_file)
-    else:
-        os.makedirs('data', exist_ok=True)
-        inverter_historical_data["micro_inverters"] = {}
+}
+'''
+inverter_historical_data = {}
+datafile = f'data/inverter_daily_data-{config["system_id"]}.json'
+if os.path.isfile(datafile) and os.access(datafile, os.R_OK):
+    with open(datafile) as json_file:
+        inverter_historical_data = json.load(json_file)
+else:
+    os.makedirs('data', exist_ok=True)
+    inverter_historical_data["micro_inverters"] = {}
 
-    # Load the inverter data to the dictionary. 
-    yesterday = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
-    for inverter in inverter_summary["micro_inverters"]:
-        inverter_sn = str(inverter["serial_number"])
-        if inverter_sn not in inverter_historical_data["micro_inverters"]:
-            inverter_historical_data["micro_inverters"][inverter_sn] = {}
-        inverter_historical_data["micro_inverters"][inverter_sn][yesterday] = { "daily_energy": 0, "lifetime_energy": inverter["energy"]["value"]}
+# Load the inverter data to the dictionary. 
+yesterday = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
+for inverter in inverter_summary["micro_inverters"]:
+    inverter_sn = str(inverter["serial_number"])
+    if inverter_sn not in inverter_historical_data["micro_inverters"]:
+        inverter_historical_data["micro_inverters"][inverter_sn] = {}
+    inverter_historical_data["micro_inverters"][inverter_sn][yesterday] = { "daily_energy": 0, "lifetime_energy": inverter["energy"]["value"]}
 
-    # Populate the daily_energy for each inverter for today's date based on the previous day's lifetime_energy and now's lifetime_energy
-    two_days_ago = (datetime.datetime.now() + datetime.timedelta(days=-2)).strftime('%Y-%m-%d')
-    total_daily_wh = 0
-    for inverter_sn, inverter_data in inverter_historical_data["micro_inverters"].items():
-        if two_days_ago in inverter_data:
-            two_days_ago_lifetime_energy = inverter_data[two_days_ago]["lifetime_energy"]
-            yesterday_lifetime_energy = inverter_data[yesterday]["lifetime_energy"]
-            yesterday_energy = yesterday_lifetime_energy - two_days_ago_lifetime_energy
-            inverter_historical_data["micro_inverters"][inverter_sn][yesterday]["daily_energy"] = yesterday_energy
-            total_daily_wh = total_daily_wh + yesterday_energy
+# Populate the daily_energy for each inverter for today's date based on the previous day's lifetime_energy and now's lifetime_energy
+two_days_ago = (datetime.datetime.now() + datetime.timedelta(days=-2)).strftime('%Y-%m-%d')
+total_daily_wh = 0
+for inverter_sn, inverter_data in inverter_historical_data["micro_inverters"].items():
+    if two_days_ago in inverter_data:
+        two_days_ago_lifetime_energy = inverter_data[two_days_ago]["lifetime_energy"]
+        yesterday_lifetime_energy = inverter_data[yesterday]["lifetime_energy"]
+        yesterday_energy = yesterday_lifetime_energy - two_days_ago_lifetime_energy
+        inverter_historical_data["micro_inverters"][inverter_sn][yesterday]["daily_energy"] = yesterday_energy
+        total_daily_wh = total_daily_wh + yesterday_energy
 
-    # Write new data to file
-    with open(datafile, 'w') as outfile:
-        json.dump(inverter_historical_data, outfile)
+# Write new data to file
+with open(datafile, 'w') as outfile:
+    json.dump(inverter_historical_data, outfile)
 
-    print(f'Yesterdays\'s Total Energy: {total_daily_wh}Wh')
+print(f'Yesterdays\'s Total Energy: {total_daily_wh}Wh')
 
-    populate_google_sheet.run(config)
+populate_google_sheet.run(config)
 
 print('Complete...')
---
+++ utils/enlightenAPI_v4.py
@@ -0,0 +1,105 @@
+# Author:   Daniel Patenaude
+# Date:     10/13/2020
+# Desc:     API utilities for calling the Enphase Enlighten API v4
+
+import datetime
+import json
+import requests
+from base64 import b64encode
+from datetime import date
+
+class enlightenAPI_v4:
+
+    def __assert_success(self, res, exit_on_failure = True):
+        '''
+        Determine if the web request was successful (HTTP 200)
+            Returns:
+                If exit_on_failure, returned whether the web request was successful
+        '''
+        if res.status_code != 200:
+            dl.error("Server Responded: " + str(res.status_code) + " - " + res.text)
+            if exit_on_failure:
+                quit()
+            else:
+                return False
+        return True
+
+    def __log_time(self):
+        return datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S') + ": "
+
+    def fetch_systems(self):
+        '''
+        Run the enlighten API Fetch Systems route
+            Returns:
+                Returns a list of systems for which the user can make API requests. By default, systems are returned in batches of 10. The maximum size is 100.
+        '''
+        url = f'{self.config["api_url"]}api/v4/systems/?key={self.config["app_api_key"]}'
+        response = requests.get(url, headers={'Authorization': 'Bearer ' + self.config["access_token"]})
+        self.__assert_success(response)
+        result = json.loads(result.text)
+        return result
+
+    def __refresh_access_token(self):
+        '''
+        Refresh the access_token (1 day expiration) using the refresh_token (1 week expiration) using the steps detailed 
+        at: https://developer-v4.enphase.com/docs/quickstart.html#step_10.
+        This will override the current self.config and save the new config to local disk to ensure we have the latest access
+        and refresh tokens for the next use.
+        
+        Note: It's unclear from the Enlighten API docs how to refresh the refresh_token once it expires. If the refresh_token expires 
+        we're unable to call this route. Generating an access/refresh token via the API (https://developer-v4.enphase.com/docs/quickstart.html#step_8) 
+        seems to only be usable once per app auth_code.
+            Returns:
+                The full web request result of the token refresh
+        '''
+        print(self.__log_time() + "Refreshing access_token...")
+        url = f'{self.config["api_url"]}/oauth/token?grant_type=refresh_token&refresh_token={self.config["refresh_token"]}'
+        # Enlighten API v4 Quickstart says this should be a GET request, but that seems to be incorrect. POST works.
+        response = requests.post(url, auth=(self.config['app_client_id'], self.config['app_client_secret']))
+        refresh_successful = self.__assert_success(response, False)
+        if not refresh_successful:
+            print("Unable to refresh access_token. Please set a new access_token and refresh_token in the enlighten_v4_config.json. Quitting...")
+            quit()
+
+        result = json.loads(response.text)
+        
+        self.config['access_token'] = result['access_token']
+        self.config['refresh_token'] = result['refresh_token']
+
+        with open('enlighten_v4_config.json', 'w') as f:
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+
+        return result
+
+    def inverter_summary(self):
+        '''
+        Run the enlighten API inverters_summary_by_envoy_or_site route (https://developer-v4.enphase.com/docs.html).
+        This route returns the detailed information for each inverter (including lifetime power produced). Note: if your Envoy is connected via low
+        bandwidth Cellular, data only refreshes to Enlighten every 6 hours. So perform this route the next day in the early morning to ensure you get
+        complete data.
+            Returns:
+                Returns the microinverters summary based on the specified active envoy serial number or system.
+        '''
+        print(self.__log_time() + "Pulling EnlightenAPI inverter summary...")
+        url = f'{self.config["api_url"]}api/v4/systems/inverters_summary_by_envoy_or_site?key={self.config["app_api_key"]}&site_id={self.config["system_id"]}'
+        response = requests.get(url, headers={'Authorization': 'Bearer ' + self.config["access_token"]})
+        self.__assert_success(response)
+        result = json.loads(response.text)
+        return result
+
+    def __init__(self, config):
+        '''
+        Initialize the englightAPI class
+            Parameters:
+                The API configuration (as a dictionary). Must contain api_url, api_key, and secrets
+        '''
+        self.config = config
+
+        # It seems the v4 API allows you to only call the OAuth POST route with grant_type=authorization_code a SINGLE time for a auth_code.
+        # So we need to make sure those already exist.
+        if not self.config["access_token"] or not self.config["refresh_token"]:
+            print('Error: access_token or refresh_token not set in the enlighten_v4_config.json')
+            quit()
+
+        # Refresh and save out the new config with the refreshed access_token/refresh_token
+        self.__refresh_access_token()
